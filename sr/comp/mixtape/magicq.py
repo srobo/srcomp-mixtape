a6c68fcb50a9cc805849adc9534ae4b1e74dd4ab
WillB97 <will@rudi.org.uk>
2022-08-13 19:14:46 +0100

    Change OSC args

--- sr/comp/mixtape/magicq.py
+++ sr/comp/mixtape/magicq.py
@@ -9,10 +9,10 @@ class MagicqController:
         self.osc_client = SimpleUDPClient(host, port)
 
     def activate_playback(self, playback: int) -> None:
-        self.osc_client.send_message(f'/pb/{playback}/go', None)
+        self.osc_client.send_message(f'/pb/{playback}/go', [])
 
     def release_playback(self, playback: int) -> None:
-        self.osc_client.send_message(f'/pb/{playback}/release', None)
+        self.osc_client.send_message(f'/pb/{playback}/release', [])
 
     def jump_to_cue(self, playback: int, cue_id: Union[int, float, str]) -> None:
-        self.osc_client.send_message(f'/pb/{playback}/{cue_id}', None)
+        self.osc_client.send_message(f'/pb/{playback}/{cue_id}', [])
